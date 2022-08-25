b4de2ef937df2cde790ce832acf28a5577678911
ale2trelew <ale2trelew@gmail.com>
2022-08-25 11:50:21 -0300

    recuperando codigo corrupto, modelos y db hechos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,30 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    idApi: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      unique: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    release: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
     },
   });
 };
